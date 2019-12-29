5fbb4ee499ecce72b79ee4c249e91354d3c56488
vasanthkalli <vasanth.kalli@gmail.com>
2019-12-29 11:43:25 +0530

    one to many

---
+++ src/main/java/com/example/demo/entities/Employee.java
@@ -0,0 +1,64 @@
+package com.example.demo.entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
+public class Employee {
+    
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	
+	private String name;
+	
+	private int age;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	private List<Address> address;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public List<Address> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+
+
+	
+}
